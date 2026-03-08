0777c42c674ddf890a84cae18c46f2dff396192b
佳辉 宋 <3794007525@qq.com>
2026-03-08 16:16:46 +0800

    feat:增加了可以再手机端App或者电脑端desktop打开的feat

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
+  plugins: [react(),tailwindcss(),VitePWA({
+    registerType:'autoUpdate',
+    manifest:{
+      name:'我的专业番茄钟'，
+      short_name:'番茄钟',
+      description:'专注每一刻的效率工具',
+      theme_color:'#ef4444',
+      icons:[
+        {
+          src:'pomodoro.png',
+          sizes:'192*192',
+          type:'image/png'
+        },
+        {
+          src:'pomodoro.png',
+          sizes:'512*512',
+          type:'image/png'
+        }
+      ]
+
+    }
+  })]
 })
